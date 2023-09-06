2d10fc54133649932deb15242c96428b649fbe13
AndreVKF <andrekv87@gmail.com>
2023-09-06 10:29:02 -0300

    2023-09-06 - Added keydown functionality

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import "./Calculator.css";
+import React, { Component, ReactDOM } from "react"
+import "./Calculator.css"
 
-import Button from "../components/Button";
-import Display from "../components/Display";
+import Button from "../components/Button"
+import Display from "../components/Display"
 
 const initialState = {
   displayValue: "0",
@@ -10,14 +10,16 @@ const initialState = {
   operation: null,
   values: [0, 0],
   currentValuePosition: 0,
-};
+}
 
 export class Calculator extends Component {
-  state = { ...initialState };
+  state = { ...initialState }
+
+  operationsArray = ["/", "*", "-", "+", "="]
 
   clearMemory() {
     // erase calculator
-    this.setState({ ...initialState });
+    this.setState({ ...initialState })
   }
 
   setOperation(operation) {
@@ -27,22 +29,31 @@ export class Calculator extends Component {
         operation,
         currentValuePosition: 1,
         clearDisplay: true,
-      });
+      })
     } else {
-      const equals = operation === "=";
-      const currentOperation = this.state.operation;
+      const equals = operation === "="
+      const currentOperation = this.state.operation
 
       // assert operation
-      let values = [...this.state.values];
+      let values = [...this.state.values]
       try {
-        values = [eval(`${values[0]} ${currentOperation} ${values[1]}`), 0];
-        if (isNaN(values[0]) || isFinite(values[0])) {
-          this.setState({ ...initialState });
-          return;
+        values = [eval(`${values[0]} ${currentOperation} ${values[1]}`), 0]
+        if (isNaN(values[0]) || !isFinite(values[0])) {
+          this.setState({ ...initialState })
+          return
         }
       } catch (e) {
-        values = [...this.state.values];
+        values = [...this.state.values]
       }
+
+      console.log(this.state)
+      console.log({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        currentValuePosition: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      })
 
       // update state
       this.setState({
@@ -52,27 +63,27 @@ export class Calculator extends Component {
         currentValuePosition: equals ? 0 : 1,
         clearDisplay: true,
         values,
-      });
+      })
     }
   }
 
   addDigit(digit) {
     // . management
-    if (digit === "." && this.state.displayValue.includes(".")) return;
+    if (digit === "." && this.state.displayValue.includes(".")) return
 
     // case calc init => disconsider 0 input
     const clearDisplay =
-      this.state.displayValue === "0" || !!this.state.clearDisplay;
-    const currentValue = clearDisplay ? "" : this.state.displayValue;
-    const displayValue = currentValue + digit;
+      this.state.displayValue === "0" || !!this.state.clearDisplay
+    const currentValue = clearDisplay ? "" : this.state.displayValue
+    const displayValue = currentValue + digit
 
     // store input value
-    const values = [...this.state.values];
+    const values = [...this.state.values]
     if (digit !== ".") {
-      const index = this.state.currentValuePosition;
-      const newValue = parseFloat(displayValue);
+      const index = this.state.currentValuePosition
+      const newValue = parseFloat(displayValue)
 
-      values[index] = newValue;
+      values[index] = newValue
     }
 
     // update state
@@ -81,16 +92,40 @@ export class Calculator extends Component {
       displayValue,
       values,
       clearDisplay: false,
-    });
+    })
   }
 
   render() {
-    const addDigit = (n) => this.addDigit(n);
-    const setOperation = (op) => this.setOperation(op);
-    const clearMemory = () => this.clearMemory();
+    const addDigit = (n) => this.addDigit(n)
+    const setOperation = (op) => this.setOperation(op)
+    const clearMemory = () => this.clearMemory()
+    const execInput = (e) => {
+      e.preventDefault()
+      let key = e.key
+
+      if (key === "Enter") {
+        key = "="
+      }
+
+      if (this.operationsArray.includes(key)) {
+        setOperation(key)
+      } else {
+        const numberInput = Number(key)
+        if (isNaN(numberInput)) {
+          return
+        }
+
+        addDigit(numberInput)
+        console.log(this.state.displayValue)
+      }
+    }
 
     return (
-      <div className="calculator">
+      <div
+        className="calculator"
+        ref={(e) => (this.main = e)}
+        onKeyDown={(e) => execInput(e)}
+      >
         <Display value={this.state.displayValue} />
         <Button label="AC" click={clearMemory} triple />
         <Button label="/" click={setOperation} operation />
@@ -110,8 +145,8 @@ export class Calculator extends Component {
         <Button label="." click={addDigit} />
         <Button label="=" click={setOperation} operation />
       </div>
-    );
+    )
   }
 }
 
-export default Calculator;
+export default Calculator
